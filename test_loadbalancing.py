f04d5ef1878c462844f1f72b5315d9d0a3798655
tkrief <t.krief@treeptik.fr>
2017-08-30 15:11:49 +0200

    Python <3

---
+++ test_loadbalancing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+import threading
+
+
+def get_url(url):
+    requests.get(url).text
+
+
+def start_spamming():
+    for j in range(5):
+        threads = []
+        for i in range(20):
+            t = threading.Thread(target=get_url, args=("http://localhost:8080/catalog/items",))
+            threads.append(t)
+            t.start()
+
+    print("Done.")
+if __name__ == '__main__':
+    start_spamming()
