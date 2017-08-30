c1c07aabb80010d58df8faa198bfa0aca7de5bb6
tkrief <t.krief@treeptik.fr>
2017-08-30 15:20:21 +0200

    Cleanup

--- test_loadbalancing.py
+++ test_loadbalancing.py
@@ -8,14 +8,15 @@ def get_url(url):
     requests.get(url).text
 
 
+
 def start_spamming():
     for j in range(5):
-        threads = []
         for i in range(20):
             t = threading.Thread(target=get_url, args=("http://localhost:8080/catalog/items",))
-            threads.append(t)
             t.start()
 
     print("Done.")
+
+
 if __name__ == '__main__':
     start_spamming()
