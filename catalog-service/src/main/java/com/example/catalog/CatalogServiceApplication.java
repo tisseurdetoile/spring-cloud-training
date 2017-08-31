f543bda0e7e5fd9062bf2b398e81209f72272341
tkrief <t.krief@treeptik.fr>
2017-08-31 15:26:23 +0200

    Spring Cloud Config Working

--- catalog-service/src/main/java/com/example/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalog/CatalogServiceApplication.java
@@ -1,12 +1,16 @@
 package com.example.catalog;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @EnableDiscoveryClient
@@ -18,5 +22,20 @@ public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+}
+
+
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
 	}
 }
