44e4d1e26916438ede1ea2c6cad5e331f89adbb4
punkstarman <bartlettstarman@gmail.com>
2017-09-04 11:27:12 +0200

    ELK Logging

--- notification-service/src/main/java/com/example/notification/controller/NotificationController.java
+++ notification-service/src/main/java/com/example/notification/controller/NotificationController.java
@@ -3,7 +3,6 @@ package com.example.notification.controller;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
