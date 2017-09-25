9783251a2728d24fc4c9484dc593fa262fd9b2a3
punkstarman <bartlettstarman@gmail.com>
2017-09-25 23:32:42 +0200

    Add Eureka service discovery

--- notification-service/src/main/java/com/example/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/example/notification/NotificationServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableRedisRepositories
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
 	public static void main(String[] args) {
--- order-service/src/main/java/com/example/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orders/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 @EnableMongoRepositories
 @EnableHypermediaSupport(type = HypermediaType.HAL)
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
